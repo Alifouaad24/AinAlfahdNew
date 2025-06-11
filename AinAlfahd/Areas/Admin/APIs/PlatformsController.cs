b957c8629d0c60113f53b82a705b271223f340be
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2025-06-11 09:43:23 -0800

    addpl

---
+++ AinAlfahd/Areas/Admin/APIs/PlatformsController.cs
@@ -0,0 +1,25 @@
+﻿using AinAlfahd.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AinAlfahd.Areas.Admin.APIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        MasterDBContext dBContext;
+        public PlatformsController(MasterDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPlatforms()
+        {
+            var plats = await dBContext.Platforms.ToListAsync();
+            return Ok(plats);
+        }
+    }
+}
---
+++ AinAlfahd/Areas/Admin/APIs/SystemController.cs
@@ -0,0 +1,25 @@
+﻿using AinAlfahd.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AinAlfahd.Areas.Admin.APIs
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        MasterDBContext dBContext;
+        public SystemController(MasterDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllSystems()
+        {
+            var sys = await dBContext.Systems.ToListAsync();
+            return Ok(sys);
+        }
+    }
+}
